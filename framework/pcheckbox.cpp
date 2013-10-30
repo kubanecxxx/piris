55a8c831fae924c79eee4f9b2af638a0806ac015
Jakub Laník - PC <kubanecxxx@gmail.com>
2013-10-30 21:40:03 +0100

    finished checkbox component

--- framework/pcheckbox.cpp
+++ framework/pcheckbox.cpp
@@ -2,22 +2,27 @@
 #include "plabel.h"
 #include "pfont.h"
 #include "pportingabstract.h"
+#include "pkeyevent.h"
+#include "ptouchevent.h"
 
 namespace piris
 {
 
 PCheckBox::PCheckBox(PWidget * par ):
-    PWidget(par)
+    PWidget(par),
+    checked(false)
 {
 }
 
 PCheckBox::PCheckBox(PWidgetProperties_t &props, PWidget *par):
-    PWidget(props,par)
+    PWidget(props,par),
+    checked(false)
 {
 }
 
 PCheckBox::PCheckBox(const PWidgetProperties_t &props, PWidget *par):
-    PWidget(props,par)
+    PWidget(props,par),
+    checked(false)
 {
 
 }
@@ -27,23 +32,41 @@ void PCheckBox::draw(PPortingAbstract *disp) const
     PFont * f = fontDelegated();
     passert(f,"font is NULL");
     PWidget::draw(disp);
-    disp->putRectangle(x(),x()+10,y()+1,y()+11,color().dark(2,1),true);
-    //if pressed
-    disp->putText(text(),x()+14,y()+height()-2,*f,textColor());
+
+    pixel_t x1,x2,y1,y2;
+    x1 = x() + 1;
+    y1 = y() + 1;
+    x2 = x() + f->height() + 2;
+    y2 = y() + f->height() + 2;
+    pixel_t pul = (f->height() + 2) / 2;
+
+    disp->putRectangle(x1,x2,y1,y2,backgroundColorDelegated().dark(2,1),true);
+    disp->putText(text(),x2+2,y()+height()-2,*f,textColorDelegated());
+
+    //paint tick
+    if (Checked())
+    {
+        disp->putLine(x1,x1+pul,y1+pul,y2,textColorDelegated().dark(2,1));
+        disp->putLine(x1 + pul ,x2,y2 ,y1,textColorDelegated().dark(2,1));
+    }
 }
 
 void PCheckBox::processEvent(PKeyEvent *key, PTouchEvent *touch)
 {
     PWidget::processEvent(key,touch);
 
-    //press
+    if ((key->key == kENTER && key->event == PRESSED) || (touch->event == PRESS))
+    {
+        checked = !checked;
+        qlog(QString("check! %1").arg(checked));
+    }
 }
 
 void PCheckBox::recomputeGeometry()
 {
     PFont * f = fontDelegated();
     passert(f,"font is NULL");
-    setWidth(f->width()*strlen(text())+15);
+    setWidth(f->width()*strlen(text())+f->height()+6);
     setHeight(f->height()+4);
 }
 
