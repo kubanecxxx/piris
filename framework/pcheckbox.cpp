8f68ba3771ae27ff2de741c3b2397f80b3c05d27
Jakub Laník - PC <kubanecxxx@gmail.com>
2015-12-15 19:10:33 +0100

    small fixes added cmakelists

--- framework/pcheckbox.cpp
+++ framework/pcheckbox.cpp
@@ -58,7 +58,7 @@ void PCheckBox::processEvent(PKeyEvent *key, PTouchEvent *touch)
     if ((key->key == kENTER && key->event == PRESSED) || (touch->event == PRESS))
     {
         checked = !checked;
-#if DRAWING_MODE_CHANGESONLY
+#ifdef DRAWING_MODE_CHANGESONLY
         dirty = true;
 #endif
         qlog(QString("check! %1").arg(checked));
